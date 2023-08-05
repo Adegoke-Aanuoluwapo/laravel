b4c1960cc9c515b9b9b1d5017bcf7f59a0b94a80
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-08-05 09:30:51 +0100

    great

--- test-app/routes/web.php
+++ test-app/routes/web.php
@@ -23,6 +23,11 @@ Route::get('/hello', function () {
     ->header('foo', 'bar');
 });
 Route::get('/posts/{id}', function($id){
+   
     return response('Post' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request){
+
 });
 
