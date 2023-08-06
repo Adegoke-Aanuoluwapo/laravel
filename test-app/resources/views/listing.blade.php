ded1ed3efbae2ccbc17785ed66f17d000d3b5524
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-08-06 18:09:28 +0100

    grit

--- test-app/resources/views/listing.blade.php
+++ test-app/resources/views/listing.blade.php
@@ -2,4 +2,13 @@
 
 <h1>{count($listings)}</h1>
 
-@unless{count()}
+@unless{count($listing)==0}
+
+
+
+
+l@foreach($listings as $listing)
+<h2>
+ {{$listing['title']}}
+</h2>
+<p>{{$listing['discription']}}</p>
