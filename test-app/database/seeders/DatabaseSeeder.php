7c6f5fd8a767b46be128fc97801b2cb38fea7f43
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-08-24 16:17:18 +0100

    gritiful

--- test-app/database/seeders/DatabaseSeeder.php
+++ test-app/database/seeders/DatabaseSeeder.php
@@ -15,24 +15,25 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
           \App\Models\User::factory(5)->create();
-          Listing::create([
-            'title' => 'Laravel Senior Developer',
-            'tags' => 'laravel, javascript',
-            'company' => 'Acme Corp',
-            'location'=> 'Boston, MA',
-            'email' =>'Aanummaculate@gamil.com',
-            'website' => 'https://ww.acme.com',
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-          ]);
-           Listing::create([
-            'title' => 'Full Stack Engineer',
-            'tags' => 'laravel, backend, epi',
-            'company' => 'Stark Industries',
-            'location' => 'New York, Ny',
-            'email' => 'Adegoke.oye.aanuoluwapo@gamil.com',
-            'website' => 'https://www.starkindustries.com',
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-           ]);
+          Listing::factory(6)->create();
+        //   Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location'=> 'Boston, MA',
+        //     'email' =>'Aanummaculate@gamil.com',
+        //     'website' => 'https://ww.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        //   ]);
+        //    Listing::create([
+        //     'title' => 'Full Stack Engineer',
+        //     'tags' => 'laravel, backend, epi',
+        //     'company' => 'Stark Industries',
+        //     'location' => 'New York, Ny',
+        //     'email' => 'Adegoke.oye.aanuoluwapo@gamil.com',
+        //     'website' => 'https://www.starkindustries.com',
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        //    ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
