09069dc58056719868d339ddb1192bf39a4fecf4
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-08-24 15:41:56 +0100

    acme

--- test-app/app/Models/Listing.php
+++ test-app/app/Models/Listing.php
@@ -1,34 +1,11 @@
-<?php 
+<?php
+
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Listing {
-
-
- public static function all() {
- return[
-   
-  [
-                'id' => 1,
-                'title' => 'Ake Years of Chilhood',
-                'description' => 'Book written by Prof Wole Soyinka'
-
-            ],
-            [
-                'id' => 2,
-                'title' => 'Things fall apart',
-                'description' => 'Book written by Chinua Achebe'
-
-            ]
-
-            ]; }
-
-         public static function find($id){
-            $listings = self::all();
-            foreach($listings as $listing){
-                if($listing['id'] == $id){
-                    return $listing;
-                }
-            }
-         }   
+class Listing extends Model
+{
+    use HasFactory;
 }
