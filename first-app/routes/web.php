f7cbecce20c5513a7b3ebdcb69f00d33c9e6998c
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-08-25 13:22:19 +0100

    great

--- first-app/routes/web.php
+++ first-app/routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function(){
+    return 'Hello lara';
+});
+Route::get('/menu', function(){
+    return '<h2>Our Menu are great<h2>';
+});
+Route::get('/store', function () {
+    $category = request('category');
+    if (isset($category)) {
+        return 'you are viewing the store' . ' ' . strip_tags($category);
+    }
+    return 'you are viewing all instruments';
+}); 
+// Route::get('/store', function(){
+//     $category = request('category');
+//     if(isset($category)){
+//         return 'you are viewing the store'. ' '. strip_tags($category);
+//     }
+//     return 'you are viewing all instruments';
+    
+// }); 
