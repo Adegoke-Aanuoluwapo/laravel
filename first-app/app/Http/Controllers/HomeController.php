aa4583101ac949726443f811f12e38932c5df993
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-08-25 14:25:09 +0100

    more

--- first-app/app/Http/Controllers/HomeController.php
+++ first-app/app/Http/Controllers/HomeController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index() {
+         return '<h1>You are Welcome </h1>';
+    }
+    public function about()
+    {
+        return view('about');
+    }
+    public function contact()
+    {
+        return '<h2>contact page </h2>';
+    }
 }
