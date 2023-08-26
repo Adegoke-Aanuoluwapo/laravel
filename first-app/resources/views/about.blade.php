e66e5e0aa41f8ffde4c5dd460639dd4c4c395c3c
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-08-26 12:57:51 +0100

    great

--- first-app/resources/views/about.blade.php
+++ first-app/resources/views/about.blade.php
@@ -1,5 +1,5 @@
 @extends('layout')
-
+@section('title', 'About Us')
 @section('content')
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
@@ -14,4 +14,8 @@
                 
             </div>
 @endsection
-  
+@section('scripts')
+<script>
+    alert('About Us')
+</script>
+@endsection
