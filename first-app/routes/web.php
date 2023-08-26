c1d9e14c6ff1cfff99b42bb59a94c88d48712559
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-08-26 13:40:38 +0100

    make sense

--- first-app/routes/web.php
+++ first-app/routes/web.php
@@ -13,9 +13,9 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'] );
-Route::get('/about', [HomeController::class, 'about'] );
-Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/', [HomeController::class, 'index'] )->name('home.index');
+Route::get('/about', [HomeController::class, 'about'])->name('home.about');;
+Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');;
 Route::get('/menu', function(){
     return '<h2>Our Menu are great<h2>';
 });
