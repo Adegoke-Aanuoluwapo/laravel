3d61c3d55a9252889a721812957451080f79e1a0
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-08-26 12:34:11 +0100

    great

--- first-app/resources/views/welcome.blade.php
+++ first-app/resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
         <div class="relative p-4">
       <nav>
         <a href="/">Home</a>
-      <a href="/about/about">About</a>
+      <a href="/about">About</a>
       <a href="/contact">Contact</a>
       </nav>      
      </div>
