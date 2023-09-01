92860c0bd1d7257c2a7220ca9e3397cb6bf08128
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-09-01 17:05:49 +0100

    more grit

--- first-app/resources/views/guitars/create.blade.php
+++ first-app/resources/views/guitars/create.blade.php
@@ -7,7 +7,7 @@
   <div>
    <label class="text-sm" for="guitar-name">Guitar Name</label>
    <input class="text-lg border-1" type="text" id="guitar-name" name="guitar-name">
-   @error('guitat-name')
+   @error('guitar-name')
     <div class="form-error">
       {{$message}}
     </div>
@@ -16,10 +16,20 @@
   <div>
    <label class="text-sm" for="brand">Brand</label>
    <input class="text-lg border-1" type="text" id="brand" name="brand">
+    @error('brand')
+    <div class="form-error">
+      {{$message}}
+    </div>
+   @enderror
   </div>
   <div>
    <label class="text-sm" for="year">Year Made</label>
-   <input class="text-lg border-1" type="text" id="year" name="year">
+   <input class="text-lg border-1" type="text" id="year" name="year_made">
+    @error('year_made')
+    <div class="form-error">
+      {{$message}}
+    </div>
+   @enderror
   </div>
   <div>
    <button class="border-1" type="submit">Submit</button>
