b81723edf903304bc3b504ff65fac3b193d80978
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-09-06 18:33:16 +0100

    great

--- food-app/routes/web.php
+++ food-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Process;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function (){
+   return Process::run('ls - la')-> output();
 });
